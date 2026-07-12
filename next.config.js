54abc49b9de426dd5218194b0cd4e5fcac5cdd7c
derekhuit-afk <derekhuit@gmail.com>
2026-07-12 13:10:59 +0000

    redirect: 301 all paths to akmilitaryhomeloans.com

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://www.akmilitaryhomeloans.com/:path*",
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
